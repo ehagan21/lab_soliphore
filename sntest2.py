ac4ec8d05152bf67a7135a1b7219b922e541186d
ehagan21 <ehagan21@gmail.com>
2019-05-22 17:12:13 +0100

    serial progress

--- sntest2.py
+++ sntest2.py
@@ -2,21 +2,22 @@
 import serial
 import time
 import json
+import re
 from newsapi import NewsApiClient
 
 #loading credentials
-with open('config.json') as json_data_file:
+with open('/home/pi/lab/config.json') as json_data_file:
     data = json.load(json_data_file)
 
 #serial port information
 port = "/dev/ttyACM0"
 rate = 115200
 ser = serial.Serial(port,rate)
+headlines = []
+
 if ser.isOpen():
     print('success')
-
-#test string
-string1 = b'finally'
+    ser.write('A')
 
 #Init
 news_key = data["api_key"]
@@ -25,23 +26,33 @@ newsapi = NewsApiClient(api_key=news_key)
 top_headlines = newsapi.get_top_headlines(language='en', country ='us')
 #all_articles = newsapi.get_everything(q='flag', language = 'en', sort_by='relevancy')
 
-#for item in all_articles["articles"]:
-#   ser.write(item["title"].encode('utf-8')
+def sanitize():
+    for item in top_headlines["articles"]:
+        xstring = item["title"].encode('utf-8')
+        hlist = re.sub("[^a-zA-Z ]+", "", xstring)
+        message = '<' + hlist + '>'
+        headlines.append(message)
+        print(message)
 
-for item in top_headlines["articles"]:
-    xstring = item["title"].encode('utf-8')
-    hlist = (xstring.split('/n/r'))
 
-print(xstring)
+#keeps track of which headline we should be playing
+position = 0
 
-ser.write(xstring)
+sanitize()
 
+#START THE LOOPING DATA SENT
 while True:
     if (ser.in_waiting>0):
-        a = ser.readline()
-        print(a)
-        for i in range(len(hlist)):
-            print(hlist[i])
-            ser.write(hlist[i])
-            time.sleep(1)
+        data = ser.read()
+        data_left = ser.inWaiting()
+        print(data_left)
+        print(headlines[position])
+        ser.write(headlines[position])
+        time.sleep(5)
 
+        #loop through the headlines to play all necessary
+        if (position < len(headlines)):
+            position += 1
+        else:
+            print('done')
+            break
