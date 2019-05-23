5064bd9470200c53fa3cb9dfb6a639282dd3062f
ehagan21 <ehagan21@gmail.com>
2019-05-23 05:20:47 +0100

    eod progress 522

--- sntest2.py
+++ sntest2.py
@@ -3,6 +3,7 @@ import serial
 import time
 import json
 import re
+import atexit
 from newsapi import NewsApiClient
 
 #loading credentials
@@ -12,12 +13,8 @@ with open('/home/pi/lab/config.json') as json_data_file:
 #serial port information
 port = "/dev/ttyACM0"
 rate = 115200
-ser = serial.Serial(port,rate)
+ser = serial.Serial(port,rate, timeout = 0)
 headlines = []
-
-if ser.isOpen():
-    print('success')
-    ser.write('A')
 
 #Init
 news_key = data["api_key"]
@@ -30,35 +27,45 @@ def sanitize():
     for item in top_headlines["articles"]:
         xstring = item["title"].encode('utf-8')
         hlist = re.sub("[^a-zA-Z ]+", "", xstring)
-        message = '<' + hlist + '>'
+        message = '<' + hlist.upper() + '>'
         headlines.append(message)
-        print(message)
-
+        #print(message)
 
 #keeps track of which headline we should be playing
 position = 0
 
 sanitize()
 
-ser.flushInput()
+if (ser.in_waiting>0):
+    ser.write('A')
+
+def exit_handler():
+    print("closing application")
+    ser.flushOutput()
+    ser.close()
 
 #START THE LOOPING DATA SENT
-while True:
-    data = ser.out_waiting
-    print(data)
-    if (ser.out_waiting>0):
-        time.delay(2)
-    else:
-        if (ser.in_waiting>0):
+try:
+    while True:
+        if (ser.out_waiting>0):
+            time.sleep(2)
+        elif (ser.in_waiting>0):
             data = ser.read()
             print(headlines[position])
             ser.write(headlines[position])
 
         #loop through the headlines to play all necessary
-        #if (position < len(headlines)-1):
+            #if (position < len(headlines)-1):
             if (position < 2):
                 position += 1
             else:
+                position = 0
                 print('done')
                 #break
-            time.sleep(5)
+
+except KeyboardInterrupt:
+    #cleanup
+    exit_handler()
+
+except:
+    exit_handler()
