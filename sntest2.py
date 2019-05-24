72c28ac4da842b49b51af00b01e9c96d82e12b6a
ehagan21 <ehagan21@gmail.com>
2019-05-24 15:18:57 +0100

    fixing headline strings

--- sntest2.py
+++ sntest2.py
@@ -23,26 +23,28 @@ newsapi = NewsApiClient(api_key=news_key)
 top_headlines = newsapi.get_top_headlines(language='en', country ='us')
 #all_articles = newsapi.get_everything(q='flag', language = 'en', sort_by='relevancy')
 
+#tracking where in the headlines array to send data
+position = 0
+
 def sanitize():
     for item in top_headlines["articles"]:
         xstring = item["title"].encode('utf-8')
         hlist = re.sub("[^a-zA-Z ]+", "", xstring)
         message = '<' + hlist.upper() + '>'
         headlines.append(message)
-        #print(message)
-
-#keeps track of which headline we should be playing
-position = 0
-
-sanitize()
-
-if (ser.in_waiting>0):
-    ser.write('A')
+        print(message)
 
 def exit_handler():
     print("closing application")
     ser.flushOutput()
     ser.close()
+
+sanitize()
+
+if (ser.in_waiting>0):
+    inData = ser.readline()
+    if (inData == 'waiting'):
+       ser.write('A')
 
 #START THE LOOPING DATA SENT
 try:
@@ -50,17 +52,18 @@ try:
         if (ser.out_waiting>0):
             time.sleep(2)
         elif (ser.in_waiting>0):
-            data = ser.read()
-            print(headlines[position])
-            ser.write(headlines[position])
+            data = ser.readline()
+            if (data == 'waiting'):
+                ser.write('A')
+            else:
+                print(data)
+                ser.write(headlines[position])
 
         #loop through the headlines to play all necessary
-            #if (position < len(headlines)-1):
-            if (position < 2):
+            if (position < len(headlines)-1):
                 position += 1
             else:
                 position = 0
-                print('done')
                 #break
 
 except KeyboardInterrupt:
