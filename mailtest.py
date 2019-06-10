38b9be5a201d3a03c2a3a2d77c3e2294303fbe91
ehagan21 <ehagan21@gmail.com>
2019-06-10 17:30:36 +0100

    grabbing body text

--- mailtest.py
+++ mailtest.py
@@ -1,21 +1,18 @@
-
-
 import smtplib
 import time
 import imaplib
 import email
+import json
 
-# -------------------------------------------------
-#
-# Utility to read email from Gmail Using Python
-#
-# ------------------------------------------------
+with open ('/home/pi/lab/config.json') as json_data_file:
+    jdata = json.load(json_data_file)
 
-ORG_EMAIL   = "@gmail.com"
-FROM_EMAIL  = "yourEmailAddress" + ORG_EMAIL
-FROM_PWD    = "yourPassword"
+FROM_EMAIL  = jdata["email"]
+FROM_PWD    = jdata["email_password"]
 SMTP_SERVER = "imap.gmail.com"
 SMTP_PORT   = 993
+
+bodytext = []
 
 def read_email_from_gmail():
     try:
@@ -24,12 +21,13 @@ def read_email_from_gmail():
         mail.select('inbox')
 
         type, data = mail.search(None, 'ALL')
+        #all emails by number, don't need unique ids
         mail_ids = data[0]
 
-        id_list = mail_ids.split()   
+        #full list of ids
+        id_list = mail_ids.split()
         first_email_id = int(id_list[0])
         latest_email_id = int(id_list[-1])
-
 
         for i in range(latest_email_id,first_email_id, -1):
             typ, data = mail.fetch(i, '(RFC822)' )
@@ -37,10 +35,21 @@ def read_email_from_gmail():
             for response_part in data:
                 if isinstance(response_part, tuple):
                     msg = email.message_from_string(response_part[1])
+                    if msg.is_multipart():
+                        for part in msg.get_payload():
+                            if part.get_content_type() == "text/plain":
+                                bodytext.append(part.get_payload().encode('UTF-8'))
+                    else:
+                        bodytext.append(msg.get_payload())
                     email_subject = msg['subject']
                     email_from = msg['from']
-                    print 'From : ' + email_from + '\n'
-                    print 'Subject : ' + email_subject + '\n'
+                    #print 'From : ' + email_from + '\n'
+                    #print 'Subject : ' + email_subject + '\n'
+                    #print msg
+
+        print bodytext
 
     except Exception, e:
         print str(e)
+
+read_email_from_gmail()
